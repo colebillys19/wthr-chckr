0c707969fc0414be60e5de8bb8ccb68026587f6c
Billy Cole <cole.billys@gmail.com>
2024-01-28 11:15:10 -0500

    Implement simple weather radar map.

--- src/Containers/HomeMap/index.tsx
+++ src/Containers/HomeMap/index.tsx
@@ -1,9 +1,126 @@
+import { CSSProperties, useEffect, useRef } from "react";
+
+import { useGlobalState } from "../../context";
+
 import { HomeSectionContainer } from "../../SharedComponentsAux";
 
-function HomeMap() {
-  //
+// https://happycoding.io/tutorials/javascript/libraries/simple-weather-map
+// https://embed.plnkr.co/ZuQneJ/
 
-  return <HomeSectionContainer>HomeMap</HomeSectionContainer>;
+const tempStyles: CSSProperties = {
+  width: "100%",
+  height: "500px",
+};
+
+//
+//
+//
+const getDateString = (date: Date) => {
+  let year = date.getUTCFullYear();
+  let month: number | string = date.getUTCMonth() + 1;
+  if (month < 10) {
+    month = "0" + month;
+  }
+  let day: number | string = date.getUTCDate();
+  if (day < 10) {
+    day = "0" + day;
+  }
+  let hour: number | string = date.getUTCHours();
+  if (hour < 10) {
+    hour = "0" + hour;
+  }
+  let minute: number | string = date.getUTCMinutes();
+  minute = minute - (minute % 5);
+  if (minute < 10) {
+    minute = "0" + minute;
+  }
+  return (
+    year.toString() +
+    month.toString() +
+    day.toString() +
+    hour.toString() +
+    minute.toString()
+  );
+};
+
+/*
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+function HomeMap() {
+  let map: google.maps.Map;
+
+  const mapRef = useRef(null);
+
+  const { googleMaps } = useGlobalState();
+
+  useEffect(() => {
+    if (googleMaps !== null && mapRef.current !== null) {
+      map = new googleMaps.Map(mapRef.current, {
+        center: { lat: 39.7, lng: -96.2 },
+        zoom: 5,
+      });
+      refreshWeatherLayer();
+      var now = new Date().getTime();
+      var msSinceLastFive = now % (5 * 60 * 1000);
+      var msUntilNextFive = 5 * 60 * 1000 - msSinceLastFive;
+      setTimeout(function () {
+        refreshWeatherLayer();
+        // refresh in 5 minutes
+        setInterval(refreshWeatherLayer, 5 * 60 * 1000);
+      }, msUntilNextFive + 10 * 100);
+    }
+  }, [googleMaps]);
+
+  //
+  //
+  //
+  const refreshWeatherLayer = () => {
+    map.overlayMapTypes.clear();
+    addWeatherFrame(0, getDateString(new Date()), 1);
+  };
+
+  //
+  //
+  //
+  const addWeatherFrame = (
+    index: number,
+    frameWeatherDate: string,
+    frameOpacity: number
+  ) => {
+    if (googleMaps !== null) {
+      const frame = new googleMaps.ImageMapType({
+        getTileUrl: function (coord, zoom) {
+          return (
+            "http://mesonet.agron.iastate.edu/cache/tile.py/1.0.0/ridge::USCOMP-N0Q-" +
+            frameWeatherDate +
+            "/" +
+            zoom +
+            "/" +
+            coord.x +
+            "/" +
+            coord.y +
+            ".png"
+          );
+        },
+        tileSize: new googleMaps.Size(256, 256),
+        opacity: frameOpacity,
+      });
+      map.overlayMapTypes.setAt(index, frame);
+    }
+  };
+
+  return (
+    <HomeSectionContainer>
+      <div ref={mapRef} style={tempStyles}></div>
+    </HomeSectionContainer>
+  );
 }
 
 export default HomeMap;
