9b8dc50f3817db1e48f0056b27dd3319a74c6b9f
Billy Cole <cole.billys@gmail.com>
2024-01-31 13:17:25 -0500

    Implement new approach for changing weather map layers.

--- src/Containers/HomeMap/TestC.tsx
+++ src/Containers/HomeMap/TestC.tsx
@@ -4,13 +4,13 @@ import { useGlobalState } from "../../context";
 import { FrameType } from "./rvTypes";
 
 const tempStyles: CSSProperties = {
-  width: "100%",
+  width: "60%",
   height: "500px",
 };
 
 function TestC() {
-  const [mapFrames, setMapFrames] = useState<FrameType[]>([]);
-  const [currentFrameIndex, setCurrentFrameIndex] = useState(-1);
+  const [currentLayerIndex, setCurrentLayerIndex] = useState(-1);
+  const [layersCount, setLayersCount] = useState(-1);
   const [error, setError] = useState("");
 
   const mapRef = useRef<google.maps.Map | null>(null);
@@ -22,72 +22,115 @@ function TestC() {
     if (googleMaps !== null && mapDivRef.current !== null) {
       mapRef.current = new googleMaps.Map(mapDivRef.current, {
         center: { lat: 40.71407, lng: -74.00019 },
-        zoom: 8,
+        zoom: 5,
       });
     }
   }, [googleMaps]);
 
   useEffect(() => {
-    if (googleMaps !== null && mapRef.current !== null) {
-      fetch("https://api.rainviewer.com/public/weather-maps.json")
-        .then((res) => {
-          if (!res.ok) {
-            throw new Error("Issue fetching RainViewer data.");
-          }
-          return res.json();
-        })
-        .then((resData) => {
-          const { host, radar } = resData;
-          const { past, nowcast } = radar;
-          const framesArray = nowcast ? past.concat(nowcast) : past;
-          setMapFrames(framesArray);
-          setCurrentFrameIndex(past.length - 1);
-          //
-          //
-          //
-          const currentFrame = framesArray[past.length - 1];
-          if (googleMaps !== null) {
-            const layerrr = new googleMaps.ImageMapType({
-              getTileUrl: function (coord, zoom) {
-                return [
-                  host + currentFrame.path,
-                  256,
-                  zoom,
-                  coord.x,
-                  coord.y,
-                  8,
-                  "1_1.png",
-                ].join("/");
-              },
-              tileSize: new googleMaps.Size(256, 256),
-              opacity: 0.001,
-            });
-            if (mapRef.current !== null) {
-              console.log(layerrr);
-              //
-              //
-              //
-              // why isn't this working?
-              mapRef.current.overlayMapTypes.push(layerrr);
-            }
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-          setError(error.message);
-        });
+    if (googleMaps === null || mapRef.current === null) {
+      return;
     }
+
+    //
+    fetch("https://api.rainviewer.com/public/weather-maps.json")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Issue fetching RainViewer data.");
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        const { host, radar } = resData;
+        const { past, nowcast } = radar;
+        const framesArr = nowcast ? past.concat(nowcast) : past;
+        if (googleMaps !== null) {
+          framesArr.forEach((frame: FrameType, i: number) => {
+            if (mapRef.current !== null) {
+              const yooo = new googleMaps.ImageMapType({
+                getTileUrl: function (coord, zoom) {
+                  return [
+                    host + frame.path,
+                    512,
+                    zoom,
+                    coord.x,
+                    coord.y,
+                    8,
+                    "1_1.png",
+                  ].join("/");
+                },
+                tileSize: new googleMaps.Size(256, 256),
+                opacity: i === past.length - 1 ? 1 : 0,
+              });
+              mapRef.current.overlayMapTypes.push(yooo);
+            }
+          });
+          setLayersCount(framesArr.length);
+          setCurrentLayerIndex(past.length - 1);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(error.message);
+      });
   }, [googleMaps]);
+
+  /*
+   *
+   */
+  // const handlePlayPauseClick = () => {};
+
+  /*
+   *
+   */
+  const handlePrevClick = () => {
+    if (mapRef.current !== null) {
+      const newLayerIndex =
+        currentLayerIndex > 0 ? currentLayerIndex - 1 : layersCount - 1;
+      const imageMapOld = mapRef.current.overlayMapTypes.getAt(
+        currentLayerIndex
+      ) as google.maps.ImageMapType;
+      const imageMapNew = mapRef.current.overlayMapTypes.getAt(
+        newLayerIndex
+      ) as google.maps.ImageMapType;
+      imageMapOld.setOpacity(0);
+      imageMapNew.setOpacity(1);
+      setCurrentLayerIndex(newLayerIndex);
+    }
+  };
+
+  /*
+   *
+   */
+  const handleNextClick = () => {
+    if (mapRef.current !== null) {
+      const newLayerIndex =
+        currentLayerIndex === layersCount - 1 ? 0 : currentLayerIndex + 1;
+      const imageMapOld = mapRef.current.overlayMapTypes.getAt(
+        currentLayerIndex
+      ) as google.maps.ImageMapType;
+      const imageMapNew = mapRef.current.overlayMapTypes.getAt(
+        newLayerIndex
+      ) as google.maps.ImageMapType;
+      imageMapOld.setOpacity(0);
+      imageMapNew.setOpacity(1);
+      setCurrentLayerIndex(newLayerIndex);
+    }
+  };
 
   if (error) {
     return <div>{error}</div>;
   }
 
-  const isLoading = !mapFrames.length || currentFrameIndex === -1;
+  const isLoading = currentLayerIndex === -1 || layersCount === -1;
 
   return (
     <>
       {isLoading && <div>Loading...</div>}
+      <div>
+        <button onClick={handlePrevClick}>prev</button>
+        <button onClick={handleNextClick}>next</button>
+      </div>
       <div ref={mapDivRef} style={tempStyles} />
     </>
   );
