e552c590343c7f0a7121bc07317b37be5c923f1d
Billy Cole <cole.billys@gmail.com>
2024-03-27 13:18:00 -0400

    Style loading state for hourly page.

--- src/Containers/PageLocationHourly/Components/Skeleton.tsx
+++ src/Containers/PageLocationHourly/Components/Skeleton.tsx
@@ -1,7 +1,15 @@
+import { Spinner } from "../../../SharedComponentsAux";
+
 function Skeleton() {
   //
 
-  return <div>skeleton</div>;
+  return (
+    <div className="pt-8 px-6 h-screen">
+      <div className="flex justify-center sm:justify-start">
+        <Spinner />
+      </div>
+    </div>
+  );
 }
 
 export default Skeleton;
