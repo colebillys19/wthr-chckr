e2343fa359a0fe64a89ca9036fcb9743616ca410
Billy Cole <cole.billys@gmail.com>
2024-03-29 09:19:06 -0400

    Implement news page.

--- src/Containers/PageCities/index.tsx
+++ src/Containers/PageCities/index.tsx
@@ -9,7 +9,7 @@ function PageCities() {
 
   return (
     <main className="pt-8 px-6 pb-36">
-      <h2 className="mb-6 text-xl">Cities</h2>
+      <h2 className="mb-8 text-xl">Cities</h2>
       <ul className="flex flex-col flex-wrap items-center gap-6 sm:flex-row">
         {citiesData.map(({ location, name }) => (
           <li key={location}>
--- src/Containers/PageNews/index.tsx
+++ src/Containers/PageNews/index.tsx
@@ -1,13 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
+import { NewsDataType } from "../../utils/types/news";
+import { parseXmlData } from "../../utils/newsHelpers";
+import { NewsList } from "../../ComponentsShared";
 function PageNews() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [data, setData] = useState<NewsDataType[]>([]);
+
+  useEffect(() => {
+    fetch("https://moxie.foxweather.com/google-publisher/weather-news.xml")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Issue fetching Fox Weather RSS.");
+        }
+        return response.text();
+      })
+      .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
+      .then((data: Document) => {
+        setData(parseXmlData(data));
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(error.message);
+        setIsLoading(false);
+      });
+  }, []);
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   return (
-    <main>
-      <h1>news</h1>
+    <main className="pt-8 px-6 pb-36 min-h-screen">
+      <h2 className="mb-8 text-xl">News</h2>
+      <NewsList isLoading={isLoading} error={error} data={data} />
     </main>
   );
 }
